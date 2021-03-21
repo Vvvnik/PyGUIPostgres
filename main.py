72d70b81d1a5ab07ebfdcff8ada32dcad073a46b
vvvnik <vvv-nik@yandex.ru>
2021-03-21 09:07:38 +0300

    Create New project )))

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Моя программа")
+    window.setGeometry(150, 100, 500, 500)
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    application()
+
